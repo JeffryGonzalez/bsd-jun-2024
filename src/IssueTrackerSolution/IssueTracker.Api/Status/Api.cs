3e2f0696c2eb8f9272fbc136e600256776666fe3
Jeff Gonzalez <jeff@hypertheory.com>
2024-06-11 11:14:30 -0400

    the records demo

--- src/IssueTrackerSolution/IssueTracker.Api/Status/Api.cs
+++ src/IssueTrackerSolution/IssueTracker.Api/Status/Api.cs
@@ -9,8 +9,17 @@ public class Api : ControllerBase
     public async Task<ActionResult> GetTheStatusAsync() // Slime
     {
         var response = new GetStatusResponse("Looks Good Here!", DateTimeOffset.UtcNow);
+        //{
+        //   Message = "Looks Good",
+        //   LastChecked = DateTimeOffset.UtcNow
+        //};
         return Ok(response); // 
     }
 }
 
 public record GetStatusResponse(string Message, DateTimeOffset LastChecked);
+//public record GetStatusResponse
+//{
+//    public required string Message { get; init; } = string.Empty;
+//    public required DateTimeOffset LastChecked { get; init; }
+//}
